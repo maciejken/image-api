6d7f5bf6288d67ca1f8dd320344376d3464352ed
Maciej Kędra <maciejken@gmail.com>
2020-10-11 19:48:09 +0200

    remove /api prefix

--- index.js
+++ index.js
@@ -56,26 +56,26 @@ const limiter = rateLimit({
 app.use(limiter);
 
 app.use(express.static('public'));
-app.get(`/api/auth`, authController.getIdToken);
-app.get(`/api/auth/verify-token`, authController.verifyIdToken);
+app.get(`/auth`, authController.getIdToken);
+app.get(`/auth/verify-token`, authController.verifyIdToken);
 
-app.get(`/api/users`, check(QueryCommon), verifyAdmin, userController.getUsers);
-app.get(`/api/users/:userId(${Regex.positiveInt})`, verifyAdmin, userController.getUser);
-app.post(`/api/users`, check(NewUserData), userController.createUser);
-app.patch(`/api/users/:userId(${Regex.positiveInt})`,
+app.get(`/users`, check(QueryCommon), verifyAdmin, userController.getUsers);
+app.get(`/users/:userId(${Regex.positiveInt})`, verifyAdmin, userController.getUser);
+app.post(`/users`, check(NewUserData), userController.createUser);
+app.patch(`/users/:userId(${Regex.positiveInt})`,
   check(UserData), verifyAdmin, userController.updateUser);
-app.delete(`/api/users/:userId(${Regex.positiveInt})`, verifyAdmin, userController.removeUser);
+app.delete(`/users/:userId(${Regex.positiveInt})`, verifyAdmin, userController.removeUser);
 
-app.get(`/api/images`, check(QueryCommon), verifyAdmin, imageController.getImages);
-app.get(`/api/images/:filename`, verifyAdmin, imageController.getImage);
-app.post('/api/images', check(NewImageData), verifyUser, imageController.createImage);
-app.patch(`/api/images/:filename`, check(ImageData), verifyAdmin, imageController.updateImage);
-app.delete(`/api/images/:filename`, verifyAdmin, imageController.removeImage);
+app.get(`/images`, check(QueryCommon), verifyAdmin, imageController.getImages);
+app.get(`/images/:filename`, verifyAdmin, imageController.getImage);
+app.post('/images', check(NewImageData), verifyUser, imageController.createImage);
+app.patch(`/images/:filename`, check(ImageData), verifyAdmin, imageController.updateImage);
+app.delete(`/images/:filename`, verifyAdmin, imageController.removeImage);
 
-app.get(`/api/uploads/:filename`, verifyUser, uploadController.getFile);
-app.get(`/api/uploads/:filename/thumbnail`, verifyUser, uploadController.getThumbnail);
-app.post(`/api/uploads`, verifyUser, upload.array(uploadField), thumbnail, uploadController.createImages);
-app.delete(`/api/uploads/:filename`, verifyAdmin, uploadController.removeImage);
+app.get(`/uploads/:filename`, verifyUser, uploadController.getFile);
+app.get(`/uploads/:filename/thumbnail`, verifyUser, uploadController.getThumbnail);
+app.post(`/uploads`, verifyUser, upload.array(uploadField), thumbnail, uploadController.createImages);
+app.delete(`/uploads/:filename`, verifyAdmin, uploadController.removeImage);
 
 const logRequestError = (req, res, next) => {
   logger.error(`${req.method} ${req.originalUrl} route not found`);
