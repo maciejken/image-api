c03f9109dd5364b718aba01ebbafe73e277e9451
Maciej Kędra <maciejken@gmail.com>
2020-11-29 17:59:53 +0100

    make crud controller and service more generic, use object prototype to create instances from opts

--- services/crud.service.js
+++ services/crud.service.js
@@ -1,79 +1,48 @@
 'use strict';
-const {
-  CvDocument,
-  Experience,
-  CvExperience,
-  Organization,
-  Skill,
-  CvSkill,
-  SkillExperience,
-} = require('../model');
 const buildQuery = require('../utils/build-query');
-const models = {
-  Experience,
-  Skill,
-};
 
-const indexNames = {
-  CvDocument: 'cvDocumentId',
-  Experience: 'experienceId',
-  Skill: 'skillId',
-};
-
-function crudify(model, linkedModels = []) {
-  const linkers = {};
-  linkedModels.map(modelName => {
-    const addFnName = `add${modelName}`;
-    linkers[addFnName] = async (id, linkedItemId) => {
-      const val = await model.findByPk(id);
-      const linkedVal = await models[modelName].findByPk(linkedItemId);
-      return val && val[addFnName] && linkedVal && val[addFnName](linkedVal);
-    };
-  });
-  return {
-    create: value => model.create(value),
-    findOne: id => model.findByPk(id),
-    findMany: ({ page, size, order }) => {
-      const query = buildQuery({ page, size, order });
-      return model.findAll(query);
-    },
-    update: async (id, value) => {
-      const val = await model.findByPk(id);
-      return val && val.update(value);
-    },
-    remove: id => model.destroy({ where: { id } }),
-    ...linkers,
-  };
+function CrudService(opts) {
+  this.create = opts.create;
+  this.readOne = opts.readOne;
+  this.readMany = opts.readMany;
+  this.update = opts.update;
+  this.destroy = opts.destroy;
 }
 
-function createLinker(model, { indexName, linkedIndexName }) {
-  return {
-    link: (id, linkedItemId) => model.create({
-      [indexName]: id,
-      [linkedIndexName]: linkedItemId,
-    }),
-    unlink: (id, linkedItemId) => model.destroy({ where: {
-      [indexName]: id,
-      [linkedIndexName]: linkedItemId,
-    }}),
-  };
+if (opts.create) {
+  CrudService.prototype.create = (value) => this.create(value);
+}
+if (opts.readOne) {
+  CrudService.prototype.readOne = (id) => this.readOne(id);
+} else {
+  CrudService.prototype.readMany = ({ page, size, order }) => {
+    const query = buildQuery({ page, size, order });
+    return this.readMany(query);
+  };   
+}
+if (opts.update) {
+  CrudService.prototype.update = async (id, value) => {
+    const val = await this.readOne(id);
+    return val && val.update(value);
+  };  
+}
+if (opts.destroy) {
+  CrudService.prototype.destroy = (id) => this.destroy({ where: { id } });
 }
 
-module.exports = {
-  cvDocument: crudify(CvDocument, ['Experience', 'Skill']),
-  organization: crudify(Organization),
-  skill: crudify(Skill),
-  experience: crudify(Experience, ['Skill']),
-  cvExperience: createLinker(CvExperience, {
-    indexName: indexNames.CvDocument,
-    linkedIndexName: indexNames.Experience,
-  }),
-  cvSkill: createLinker(CvSkill, {
-    indexName: indexNames.CvDocument,
-    linkedIndexName: indexNames.Skill,
-  }),
-  skillExperience: createLinker(SkillExperience, {
-    indexName: indexNames.Skill,
-    linkedIndexName: indexNames.Experience,
-  }),
-};
+for (let m of this.opts.linkedModels) {
+  if (m.create) {
+    CrudService.prototype[`create${m.name}`] = m.create;
+  }
+  if (m.readOne) {
+    CrudService.prototype[`readOne${m.name}`] = m.readOne;
+  } else {
+    const name = m.pluralName || `${m.name}s`; 
+    CrudService.prototype[`readMany${name}`] = m.readMany;
+  }
+  if (m.destroy) {
+    CrudService.prototype[`destroy${m.name}`] = m.destroy;
+  }
+}
+
+module.exports = CrudService;
