a6d65385c0e250ba8965cfda958125536c2fdc67
Maciej Kędra <maciejken@gmail.com>
2020-12-25 20:04:16 +0100

    use function instead of class syntax in crud controller/service

--- controllers/crud.controller.js
+++ controllers/crud.controller.js
@@ -1,101 +1,23 @@
+'use strict';
+
 const CrudService = require('../services/crud.service');
 
-module.exports = class CrudController {
-  service = null;
-  linkedModels = [];
-  identifierKey;
-  foreignKey;
-
-  constructor(settings) {
-    const { model, linkedModels, identifierKey, foreignKey } = settings;
-    this.model = model;
-    this.linkedModels = linkedModels;
-    this.identifierKey = identifierKey || 'id';
-    this.foreignKey = foreignKey;
-    this.service = new CrudService(settings);
-    
-    if (linkedModels) {
-      for (const lm of linkedModels) {
-        const targetKey = lm.targetKey || 'id';
-        const getParams = (req) => ({
-          foreignKey: {
-            name: this.foreignKey,
-            value: req.params[this.identifierKey]
-          },
-          targetKey: {
-            name: targetKey,
-            value: req.params[lm.otherKey],
-          },
-          otherKey: {
-            name: lm.otherKey,
-            value: req.params[lm.otherKey],
-          },
-          value: req.body,
-        });
-        this[`create${lm.modelName}s`] = async (req, res, next) => {
-          try {
-            const params = getParams(req);
-            if (Array.isArray(req.body)) {
-              const data = await this.service[`create${lm.modelName}s`](params);
-              res.status(201).json(data);
-            } else {
-              const [item, isNew] = await this.service[`create${lm.modelName}`](params);
-              res.status(isNew ? 201 : 200).json(item);
-            }
-          } catch (err) {
-            next(err);
-          }
-        };
-        this[`get${lm.modelName}`] = async (req, res, next) => {
-          try {
-            const params = getParams(req);
-            const item = await this.service[`get${lm.modelName}`](params);
-            res.status(200).json(item);
-          } catch (err) {
-            next(err);
-          }
-        };
-        this[`get${lm.modelName}s`] = async (req, res, next) => {
-          try {
-            const params = getParams(req);
-            const items = await this.service[`get${lm.modelName}s`](params);
-            res.status(200).json(items);
-          } catch (err) {
-            next(err);
-          }
-        };
-        this[`update${lm.modelName}`] = async (req, res, next) => {
-          try {
-            const params = getParams(req);
-            const updatedItem = await this.service[`update${lm.modelName}`](params);
-            res.status(200).json(updatedItem);
-          } catch (err) {
-            next(err);
-          }
-        };
-        this[`remove${lm.modelName}`] = async (req, res, next) => {
-          try {
-            const params = getParams(req);
-            const result = await this.service[`remove${lm.modelName}`](params);
-            res.status(200).json(result);
-          } catch (err) {
-            next(err);
-          }
-        };
-      }
-    }
-  }
-
-  create = async (req, res, next) => {
+function CrudController(settings) {
+  const { linkedModels, identifierKey, foreignKey } = settings;
+  this.linkedModels = linkedModels || [];
+  this.identifierKey = identifierKey || 'id';
+  this.foreignKey = foreignKey;
+  this.service = new CrudService(settings);
+  this.create = async (req, res, next) => {
     try {
+      console.log('this', this);
       const createdItem = await this.service.create(req.body);
       res.status(200).json(createdItem);
     } catch (err) {
       next(err);
     }
-  }
-
-  getMany = async (req, res, next) => {
+  };
+  this.getMany = async (req, res, next) => {
     try {
       const { order, page, size } = req.query;
       const items = await this.service.getMany({ order, page, size });
@@ -103,33 +25,103 @@ module.exports = class CrudController {
     } catch (err) {
       next(err);
     }  
-  }
-
-  getOne = async (req, res, next) => {
+  };
+  this.getOne = async (req, res, next) => {
     try {
       const item = await this.service.getOne(req.params[this.identifierKey]);
       res.status(200).json(item);      
     } catch (err) {
       next(err);
     }
-  }
-
-  update = async (req, res, next) => {
+  };
+  this.update = async (req, res, next) => {
     try {
       const updatedItem = await this.service.update(req.params[this.identifierKey], req.body);
       res.status(200).json(updatedItem);
     } catch (err) {
       next(err);
     }
-  }
-
-  remove = async (req, res, next) => {
+  };
+  this.remove = async (req, res, next) => {
     try {
       const result = await this.service.remove(req.params[this.identifierKey]);
       res.status(200).json(result);
     } catch (err) {
       next(err);
     }
-  }
+  };
+  generateLinkedModelMethods(this, linkedModels);
+}
 
-};
+function generateLinkedModelMethods(ctx, linkedModels) {
+  for (const lm of linkedModels) {
+    const targetKey = lm.targetKey || 'id';
+    const getParams = (req) => ({
+      foreignKey: {
+        name: ctx.foreignKey,
+        value: req.params[ctx.identifierKey]
+      },
+      targetKey: {
+        name: targetKey,
+        value: req.params[lm.otherKey],
+      },
+      otherKey: {
+        name: lm.otherKey,
+        value: req.params[lm.otherKey],
+      },
+      value: req.body,
+    });
+    ctx[`create${lm.modelName}s`] = async (req, res, next) => {
+      try {
+        const params = getParams(req);
+        if (Array.isArray(req.body)) {
+          const data = await ctx.service[`create${lm.modelName}s`](params);
+          res.status(201).json(data);
+        } else {
+          const [item, isNew] = await ctx.service[`create${lm.modelName}`](params);
+          res.status(isNew ? 201 : 200).json(item);
+        }
+      } catch (err) {
+        next(err);
+      }
+    };
+    ctx[`get${lm.modelName}`] = async (req, res, next) => {
+      try {
+        const params = getParams(req);
+        const item = await ctx.service[`get${lm.modelName}`](params);
+        res.status(200).json(item);
+      } catch (err) {
+        next(err);
+      }
+    };
+    ctx[`get${lm.modelName}s`] = async (req, res, next) => {
+      try {
+        const params = getParams(req);
+        const items = await ctx.service[`get${lm.modelName}s`](params);
+        res.status(200).json(items);
+      } catch (err) {
+        next(err);
+      }
+    };
+    ctx[`update${lm.modelName}`] = async (req, res, next) => {
+      try {
+        const params = getParams(req);
+        const updatedItem = await ctx.service[`update${lm.modelName}`](params);
+        res.status(200).json(updatedItem);
+      } catch (err) {
+        next(err);
+      }
+    };
+    ctx[`remove${lm.modelName}`] = async (req, res, next) => {
+      try {
+        const params = getParams(req);
+        const result = await ctx.service[`remove${lm.modelName}`](params);
+        res.status(200).json(result);
+      } catch (err) {
+        next(err);
+      }
+    };
+  }
+}
+
+module.exports = CrudController;
--- services/crud.service.js
+++ services/crud.service.js
@@ -2,157 +2,17 @@
 
 const { buildQuery } = require('../utils');
 
-module.exports = class CrudService {
-  model;
-  linkedModels;
-  identifierKey;
-
-  constructor(settings) {
-    const { model, linkedModels, identifierKey } = settings;
-    this.model = model;
-    this.linkedModels = linkedModels;
-    this.identifierKey = identifierKey || 'id';
-
-    if (linkedModels) {
-      for (const lm of linkedModels) {
-        const { through, updateOnDuplicate } = lm;
-        this[`create${lm.modelName}`] = async (params) => {
-          const { value, foreignKey, targetKey, otherKey } = params;
-          let val;
-          if (through) {
-            const resp = await lm.model.findOrCreate({
-              where: value,
-            });
-            await through.findOrCreate({
-              where: {
-                [foreignKey.name]: foreignKey.value,
-                [otherKey.name]: resp[0][targetKey.name],
-              }
-            });
-            val = resp;
-          } else {
-            val = await lm.model.findOrCreate({
-              where: { ...value, [foreignKey.name]: foreignKey.value },
-            });
-          }
-          return val;
-        };
-        this[`create${lm.modelName}s`] = async (params) => {
-          const { value, foreignKey, targetKey, otherKey } = params;
-          let data;
-          if (through) {
-            data = await lm.model.bulkCreate(value, { updateOnDuplicate });
-            const throughData = data.map(d => ({
-              [foreignKey.name]: d[foreignKey.name],
-              [otherKey.name]: d[targetKey.name],
-            }));
-            await through.bulkCreate(throughData, { ignoreDuplicates: true });
-          } else {
-            const values = value.map(v => ({
-              ...v,
-              [foreignKey.name]: foreignKey.value,
-            }));
-            data = await lm.model.bulkCreate(values, { updateOnDuplicate });
-          }
-          return data;
-        };
-        this[`get${lm.modelName}`] = async (params) => {
-          let val = null;
-          const { foreignKey, targetKey, otherKey } = params;
-          if (through) {
-            const throughInstance = await through.findOne({
-              where: {
-                [foreignKey.name]: foreignKey.value,
-                [otherKey.name]: otherKey.value,
-              }
-            });
-            val = throughInstance && await lm.model.findByPk(targetKey.value);
-          } else {
-            val = await lm.model.findOne({
-              where: {
-                [foreignKey.name]: foreignKey.value,
-                [targetKey.name]: targetKey.value,
-              },
-              include: lm.include,
-            });
-          }
-          return val;
-        };
-        this[`get${lm.modelName}s`] = async (params) => {
-          let query;
-          const { foreignKey, targetKey, otherKey } = params;
-          if (through) {
-            const throughInstances = await through.findAll({
-              where: {
-                [foreignKey.name]: foreignKey.value,
-              }
-            });
-            const ids = throughInstances.map(instance => instance[otherKey.name]);
-            query = { where: { [targetKey.name]: ids } };
-          } else {
-            query = { where: { [foreignKey.name]: foreignKey.value } };
-          }
-          return await lm.model.findAll(query);
-        };
-        this[`update${lm.modelName}`] = async (params) => {
-          const { value, foreignKey, targetKey, otherKey } = params;
-          let val;
-          if (through) {
-            const throughInstance = await through.findOne({
-              where: {
-                [foreignKey.name]: foreignKey.value,
-                [otherKey.name]: otherKey.value,
-              }
-            });
-            const instance = throughInstance && await lm.model.findByPk(targetKey.value);
-            val = instance && await instance.update(value);
-            const idChanged = val && val[targetKey.name] !== otherKey.value;
-            if (idChanged) {
-              await throughInstance.update({ [otherKey.name]: val[targetKey.name] });
-            }
-          } else {
-            const instance = await lm.model.findOne({
-              where: {
-                // keep foreignKey to ensure relation
-                [foreignKey.name]: foreignKey.value,
-                [targetKey.name]: targetKey.value,
-              }
-            });
-            val = instance && await instance.update(value);            
-          }
-          return val;
-        };
-        this[`remove${lm.modelName}`] = async (params) => {
-          const { foreignKey, targetKey, otherKey } = params;
-          let val;
-          if (through) {
-            val = await through.destroy({
-              where: {
-                [foreignKey.name]: foreignKey.value,
-                [otherKey.name]: otherKey.value,
-              }
-            });
-          } else {
-            val = await lm.model.destroy({
-              where: {
-                // keep foreignKey to ensure relation
-                [foreignKey.name]: foreignKey.value,
-                [targetKey.name]: targetKey.value,
-            }});
-          }
-          return val;
-        };
-      }    
-    }
-  }
-
-  create(value) {
+function CrudService(settings) {  
+  const { model, linkedModels, identifierKey } = settings;
+  this.model = model;
+  this.linkedModels = linkedModels || [];
+  this.identifierKey = identifierKey || 'id';
+  this.create = (value) => {
     return this.model.create(value);
-  }
-
-  getOne(id) {
+  };
+  this.getOne = (id) => {
     return this.model.findByPk(id, {
-      include: this.linkedModels
+      include: linkedModels
         .filter(lm => lm.eager)
         .map(lm => {
           const { model, through, as } = lm;
@@ -163,20 +23,151 @@ module.exports = class CrudService {
           return val;
         })
       });
-  }
-
-  getMany({ page, size, order }) {
+  };
+  this.getMany = ({ page, size, order }) => {
     const query = buildQuery({ page, size, order });
     return this.model.findAll(query);
-  }
-
-  async update(id, value) {
+  };
+  this.update = async (id, value) => {
     const instance = await this.model.findByPk(id);
     return instance && instance.update(value);
-  }
-
-  remove(id) {
+  };
+  this.remove = (id) => {
     return this.model.destroy({ where: { [this.identifierKey]: id } });
   }
-
+  generateLinkedModelMethods(this, linkedModels);
 };
+
+function generateLinkedModelMethods(ctx, linkedModels) {
+  for (const lm of linkedModels) {
+    const { through, updateOnDuplicate } = lm;
+    ctx[`create${lm.modelName}`] = async (params) => {
+      const { value, foreignKey, targetKey, otherKey } = params;
+      let val;
+      if (through) {
+        const resp = await lm.model.findOrCreate({
+          where: value,
+        });
+        await through.findOrCreate({
+          where: {
+            [foreignKey.name]: foreignKey.value,
+            [otherKey.name]: resp[0][targetKey.name],
+          }
+        });
+        val = resp;
+      } else {
+        val = await lm.model.findOrCreate({
+          where: { ...value, [foreignKey.name]: foreignKey.value },
+        });
+      }
+      return val;
+    };
+    ctx[`create${lm.modelName}s`] = async (params) => {
+      const { value, foreignKey, targetKey, otherKey } = params;
+      let data;
+      if (through) {
+        data = await lm.model.bulkCreate(value, { updateOnDuplicate });
+        const throughData = data.map(d => ({
+          [foreignKey.name]: d[foreignKey.name],
+          [otherKey.name]: d[targetKey.name],
+        }));
+        await through.bulkCreate(throughData, { ignoreDuplicates: true });
+      } else {
+        const values = value.map(v => ({
+          ...v,
+          [foreignKey.name]: foreignKey.value,
+        }));
+        data = await lm.model.bulkCreate(values, { updateOnDuplicate });
+      }
+      return data;
+    };
+    ctx[`get${lm.modelName}`] = async (params) => {
+      let val = null;
+      const { foreignKey, targetKey, otherKey } = params;
+      if (through) {
+        const throughInstance = await through.findOne({
+          where: {
+            [foreignKey.name]: foreignKey.value,
+            [otherKey.name]: otherKey.value,
+          }
+        });
+        val = throughInstance && await lm.model.findByPk(targetKey.value);
+      } else {
+        val = await lm.model.findOne({
+          where: {
+            [foreignKey.name]: foreignKey.value,
+            [targetKey.name]: targetKey.value,
+          },
+          include: lm.include,
+        });
+      }
+      return val;
+    };
+    ctx[`get${lm.modelName}s`] = async (params) => {
+      let query;
+      const { foreignKey, targetKey, otherKey } = params;
+      if (through) {
+        const throughInstances = await through.findAll({
+          where: {
+            [foreignKey.name]: foreignKey.value,
+          }
+        });
+        const ids = throughInstances.map(instance => instance[otherKey.name]);
+        query = { where: { [targetKey.name]: ids } };
+      } else {
+        query = { where: { [foreignKey.name]: foreignKey.value } };
+      }
+      return await lm.model.findAll(query);
+    };
+    ctx[`update${lm.modelName}`] = async (params) => {
+      const { value, foreignKey, targetKey, otherKey } = params;
+      let val;
+      if (through) {
+        const throughInstance = await through.findOne({
+          where: {
+            [foreignKey.name]: foreignKey.value,
+            [otherKey.name]: otherKey.value,
+          }
+        });
+        const instance = throughInstance && await lm.model.findByPk(targetKey.value);
+        val = instance && await instance.update(value);
+        const idChanged = val && val[targetKey.name] !== otherKey.value;
+        if (idChanged) {
+          await throughInstance.update({ [otherKey.name]: val[targetKey.name] });
+        }
+      } else {
+        const instance = await lm.model.findOne({
+          where: {
+            // keep foreignKey to ensure relation
+            [foreignKey.name]: foreignKey.value,
+            [targetKey.name]: targetKey.value,
+          }
+        });
+        val = instance && await instance.update(value);            
+      }
+      return val;
+    };
+    ctx[`remove${lm.modelName}`] = async (params) => {
+      const { foreignKey, targetKey, otherKey } = params;
+      let val;
+      if (through) {
+        val = await through.destroy({
+          where: {
+            [foreignKey.name]: foreignKey.value,
+            [otherKey.name]: otherKey.value,
+          }
+        });
+      } else {
+        val = await lm.model.destroy({
+          where: {
+            // keep foreignKey to ensure relation
+            [foreignKey.name]: foreignKey.value,
+            [targetKey.name]: targetKey.value,
+        }});
+      }
+      return val;
+    };
+  }    
+}
+
+module.exports = CrudService;
