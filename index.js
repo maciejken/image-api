804c4982da32215666824749bbec2cbf8d0eddb5
Maciej Kędra <maciejken@gmail.com>
2021-03-21 08:48:12 +0100

    add REDIS_PORT to redis.createClient config

--- index.js
+++ index.js
@@ -10,7 +10,10 @@ const redis = require('redis');
 const session = require('express-session');
 
 let RedisStore = require('connect-redis')(session);
-let redisClient = redis.createClient({ host: process.env.REDIS_HOST });
+let redisClient = redis.createClient({
+  host: process.env.REDIS_HOST,
+  port: process.env.REDIS_PORT,
+});
 
 const logger = require('./libs/logger')('server');
 const errorHandler = require('./middleware/errors/error-handler');
